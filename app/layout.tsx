86c1c345d799d6ea8124108ead72ca5e5ab99566
Abbas Malik <rittus528@gmail.com>
2026-04-21 04:11:39 -0400

    added tawk live chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,14 +42,14 @@ export default function RootLayout({
             }}
           />
 
-          {/* Tawk.to Embedded Snippet Logic */}
+          {/* Tawk.to Live Chat - Updated with your ID */}
           <Script id="tawk-to" strategy="afterInteractive">
             {`
               var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
               (function(){
               var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
               s1.async=true;
-              s1.src='https://embed.tawk.to/YOUR_PROPERTY_ID/default';
+              s1.src='https://embed.tawk.to/69e72da6d6c12b1c364ef34c/1jmngklj0';
               s1.charset='UTF-8';
               s1.setAttribute('crossorigin','*');
               s0.parentNode.insertBefore(s1,s0);
